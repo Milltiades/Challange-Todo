cdd6d6118e0559cacabdfa26671c57b346e89d4e
Milltiades <gigakakulia2017@gmail.com>
2023-04-28 21:57:18 +0400

    fixed bug 21:57

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,97 +1,97 @@
-import React, { useEffect, useState } from "react";
-import styled from "styled-components";
+// import React, { useEffect, useState } from "react";
+// import styled from "styled-components";
 
-export default function test() {
+// export default function test() {
 
-  const [inputValue, setInputValue] = useState<any>("");
-  const [arr, setArr] = useState<any>([
-    {
-      value: "work 1",
-      workDone: false,
-    },
-    {
-      value: "work 2",
-      workDone: false,
-    },
-    {
-      value: "work 3",
-      workDone: false,
-    },
-    {
-      value: "work 4",
-      workDone: false,
-    },
-  ]);
-const [newArray, setNewArray] = useState([...arr]);
-  const handleCHange = (e: any) => {
-    setInputValue({
+//   const [inputValue, setInputValue] = useState<any>("");
+//   const [arr, setArr] = useState<any>([
+//     {
+//       value: "work 1",
+//       workDone: false,
+//     },
+//     {
+//       value: "work 2",
+//       workDone: false,
+//     },
+//     {
+//       value: "work 3",
+//       workDone: false,
+//     },
+//     {
+//       value: "work 4",
+//       workDone: false,
+//     },
+//   ]);
+// const [newArray, setNewArray] = useState([...arr]);
+//   const handleCHange = (e: any) => {
+//     setInputValue({
       
-      value: e.target.value,
-      workDone: false,
-    });
-  };
-  const addFunc = () => {
-    setArr([...arr, inputValue]);
-  };
-  const removeFunc = (index: any) => {
-    const newArr = [...arr];
-    newArr.splice(index, 1);
-    setArr(newArr);
-  };
+//       value: e.target.value,
+//       workDone: false,
+//     });
+//   };
+//   const addFunc = () => {
+//     setArr([...arr, inputValue]);
+//   };
+//   const removeFunc = (index: any) => {
+//     const newArr = [...arr];
+//     newArr.splice(index, 1);
+//     setArr(newArr);
+//   };
 
-  const changeColor = (index: any) => {
-    console.log(arr[index].workDone)
-   arr[index].workDone = true;
-   setNewArray([...arr]);
+//   const changeColor = (index: any) => {
+//     console.log(arr[index].workDone)
+//    arr[index].workDone = true;
+//    setNewArray([...arr]);
     
    
-  };
+//   };
   
- useEffect(() => {
-     localStorage.setItem("arr", JSON.stringify(arr))
-     const arrLocal = JSON.parse(localStorage.getItem("arr"));
-     console.log(arrLocal)
-     setNewArray(arrLocal)
- }, [arr]);
+//  useEffect(() => {
+//      localStorage.setItem("arr", JSON.stringify(arr))
+//      const arrLocal = JSON.parse(localStorage.getItem("arr"));
+//      console.log(arrLocal)
+//      setNewArray(arrLocal)
+//  }, [arr]);
 
  
 
-  return (
-    <Div>
-      <input type="text" onChange={handleCHange} />
-      <button onClick={() => addFunc()}>click me</button>
-      {newArray.map((item:any, index:any) => {
-        return (
-          <Task
-            key={index}
-            background={ item.workDone ? "red" : "blue"}
-          >
-            {item.value}
-            <>
-              <button onClick={() => changeColor(index)}>done</button>
-              <button onClick={() => removeFunc(index)}>remove</button>
-            </>
-          </Task>
-        );
-      })}
-    </Div>
-  );
-}
+//   return (
+//     <Div>
+//       <input type="text" onChange={handleCHange} />
+//       <button onClick={() => addFunc()}>click me</button>
+//       {newArray.map((item:any, index:any) => {
+//         return (
+//           <Task
+//             key={index}
+//             background={ item.workDone ? "red" : "blue"}
+//           >
+//             {item.value}
+//             <>
+//               <button onClick={() => changeColor(index)}>done</button>
+//               <button onClick={() => removeFunc(index)}>remove</button>
+//             </>
+//           </Task>
+//         );
+//       })}
+//     </Div>
+//   );
+// }
 
-const Div = styled.div`
-  width: 100%;
-  height: 100%;
-  background: white;
-`;
+// const Div = styled.div`
+//   width: 100%;
+//   height: 100%;
+//   background: white;
+// `;
 
-const Task = styled.div<any>`
-  width: 200px;
-  height: 30px;
-  background: ${(props) => props.background};
-  color: white;
-  display: flex;
-  justify-content: space-between;
-`;
+// const Task = styled.div<any>`
+//   width: 200px;
+//   height: 30px;
+//   background: ${(props) => props.background};
+//   color: white;
+//   display: flex;
+//   justify-content: space-between;
+// `;
 
 
 
